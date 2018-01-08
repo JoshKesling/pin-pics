0c9fba2ab05d1c1ba1a86fae75d73549edbb8484
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2018-01-09 00:00:50 +0800

    Add Masonry gem and additional styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require rails-ujs
+//= require masonry/jquery.masonry
 //= require turbolinks
 //= require_tree .
